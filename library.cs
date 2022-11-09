b85b1d0bd832fea5ed467ad94931d907a24df411
Chinenye Okeke Maryrose <60973945+RoseChinenye@users.noreply.github.com>
2022-11-09 08:21:52 -0800

    Create library.cs

---
+++ library.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace LiveCodingSession
+{
+    internal class LibraryManagement
+    {
+        public static void Library()
+        {
+            bool BookOptions = true;
+            while (BookOptions)
+         
+            {
+                Console.WriteLine("Choose an option to see available books from c, o, d and e");
+                string Case = Console.ReadLine().ToUpper();
+                switch (Case)
+                { 
+                    case "C":
+                        Console.WriteLine("C# books");
+                        break;
+                    case "O":
+                        Console.WriteLine(".NET books");
+                        break;
+                    case "D":
+                        Console.WriteLine("Clean Code Books");
+                        break;
+                    case "E":
+                        Console.WriteLine("EFCore Books");
+                        break;
+                    default:
+                        Console.WriteLine("Try again Later...");
+                        break;
+                     
+                }
+            }
+        }
+    }
+}
