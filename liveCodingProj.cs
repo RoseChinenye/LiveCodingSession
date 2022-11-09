40c48c0abba40ecc3a08faedc3ac163061212a2f
Chinenye Okeke Maryrose <60973945+RoseChinenye@users.noreply.github.com>
2022-11-09 12:33:41 -0800

    Create liveCodingProj.cs

---
+++ liveCodingProj.cs
@@ -0,0 +1,19 @@
+using System;
+
+using LiveCodingSession;
+namespace LiveCodingSession.Runner
+{
+ internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            // MaskedString.StringMasker();
+            //CapitalLetterIndex.CheckCapitalLetter();
+            //AuthenticationSystem.BasicAuthentication();
+            //breakAndcontinue.breakAndContinueCode();
+            //LibraryManagement.Library();
+            DateWeekAfter.dateFunction();
+
+        }
+    }
+}
