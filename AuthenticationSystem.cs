8fa724373f0cf373220bf1ba506d0b2763dea07e
Chinenye Okeke Maryrose <60973945+RoseChinenye@users.noreply.github.com>
2022-11-09 08:11:47 -0800

    Create AuthenticationSystem.cs

---
+++ AuthenticationSystem.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace LiveCodingSession
+{
+    public static class AuthenticationSystem
+    {
+         internal static void BasicAuthentication()
+        {
+
+                //Registering the user
+                Console.WriteLine("Enter Your Username");
+                string username = Console.ReadLine().ToLower();
+
+                Console.WriteLine("Enter your password");
+                string Password = Console.ReadLine();
+
+                //Logging in the user
+                Console.WriteLine("Enter username to login");
+                string userName = Console.ReadLine().ToLower();
+                    if (userName == username)
+                    {
+                        Console.WriteLine("Enter your password:");
+                     
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter your correct username: ");
+                    }
+
+                    string userPassword = Console.ReadLine();
+                    {
+                        if (userPassword == Password && userName == username)
+                        {
+                            Console.WriteLine($"Your Login was Successfully!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter a correct password");
+                        }
+                    }
+
+
+            }
+            
+        }
+}
